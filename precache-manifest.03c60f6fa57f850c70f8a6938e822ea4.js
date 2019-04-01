ccfee85c813544ba6353cc770aecbf7d6f90506b
wertusser <wertusser@gmail.com>
2019-04-01 20:26:52 +0500

    Updates

---
+++ precache-manifest.03c60f6fa57f850c70f8a6938e822ea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb214ffea4d2bb1a395",
+    "url": "/doesnt-exists/static/css/main.8d918f54.chunk.css"
+  },
+  {
+    "revision": "6fb214ffea4d2bb1a395",
+    "url": "/doesnt-exists/static/js/main.d8ec5df3.chunk.js"
+  },
+  {
+    "revision": "f33e571a8945f5fc9ab5",
+    "url": "/doesnt-exists/static/js/runtime~main.73d109f8.js"
+  },
+  {
+    "revision": "5e7cf1ff9d22206342a6",
+    "url": "/doesnt-exists/static/js/2.5d46ef4f.chunk.js"
+  },
+  {
+    "revision": "e7a29ff9b73f403ad65df1c4e886763b",
+    "url": "/doesnt-exists/index.html"
+  }
+];
